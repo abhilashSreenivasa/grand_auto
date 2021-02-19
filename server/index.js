b3dba72323733a06a5b761b1edfc6c9b70f71b5b
abhilashSreenivasa <abhilash.sreenivasa@gmail.com>
2021-02-19 11:17:28 +0530

    Login and register pages created and router is set up

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
+if(process.env.NODE_ENV!=='production')
+require('dotenv').config()
+
 const express=require('express')
 const cors=require("cors")
 const bodyParser=require('body-parser')
-console.log(express)
 const app=express()
+const mongoose=require('mongoose')
 app.use(bodyParser.json())
 app.use(cors())
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+const db=mongoose.connection
 
-
+db.on('error',(e)=>{
+    console.error(e)
+})
+db.once('open',()=>console.log("connected to mongoose"))
 app.get('/',(req,res)=>{
     res.send("Hey its a vue project")
 })
